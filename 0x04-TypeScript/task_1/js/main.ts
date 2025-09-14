ff97c756f25ca94b8e23ac2cb58a7df047abafc3
ysnbk <130864850+ysnbk@users.noreply.github.com>
2025-09-14 14:53:17 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -9,3 +9,9 @@ interface Teacher {
 interface Director extends Teacher {
   numberOfReports: number;
 }
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
